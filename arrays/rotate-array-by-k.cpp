e7431ebb104665a58aa841f1ae9b58e89d395b83
Ananya Sharma <ananyasharma3064@gmail.com>
2025-08-06 22:47:33 -0400

    rotate-array-by k placed optimized copied

--- arrays/rotate-array-by-k.cpp
+++ arrays/rotate-array-by-k.cpp
@@ -1,10 +1,10 @@
 #include <vector>
 using namespace std;
-
+#-----------------Brute----------------------
 vector<int> rotateArray(vector<int> arr, int k) {
     int length = arr.size();
     if (length == 0) return arr;
-
+ 
     k = k % length;
 
     vector<int> temp(arr.begin(), arr.begin() + k); // Store first k elements
@@ -18,4 +18,27 @@ vector<int> rotateArray(vector<int> arr, int k) {
     }
 
     return arr;
+}
+--------------------------------------------------------
+---------------Optimized--------------------------------
+void rotateArray(int arr[],int n,int d){
+    reverse(arr,arr+d);
+    reverse(arr+d,arr+n);
+    reverse(arr,arr+n);
+}
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int d;
+    cin >> d;
+    rotateArray(arr,n,d);
+    for (int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+
 }
