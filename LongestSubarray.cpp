4ac51cb94af391b53a53de47202507ebe40a69cf
Ananya Sharma <ananyasharma3064@gmail.com>
2026-01-13 22:36:08 -0500

    brute solution possibly

---
+++ LongestSubarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int longestSubarray(vector<int>& arr, int k) {
+        int largest =0;
+        for(int i =0;i<arr.size();i++){
+            int summ =0;
+            for (int j=i;j<arr.size();j++){
+                summ+=arr[j];
+                if(summ == k &&(largest<((j-i)+1))){
+                    largest = (j-i)+1;
+                }
+            }
+        }
+    return largest;
+        
+    }
+    
+};
