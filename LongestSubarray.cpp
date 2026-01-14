76b420978dc05bb62cdefe61f10c864caf242e5a
Ananya Sharma <ananyasharma3064@gmail.com>
2026-01-14 13:11:39 -0500

    optimized xor solution

--- LongestSubarray.cpp
+++ LongestSubarray.cpp
@@ -16,3 +16,13 @@ class Solution {
     }
     
 };
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xorr = 0;
+        for(int i =0;i<nums.size();i++){
+            xorr = xorr ^ nums[i];
+        }
+        return xorr;
+    }
+};
