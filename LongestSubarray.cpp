1e73f6b1ed449f45fbf81cc36b600fc7f811a21a
Ananya Sharma <ananyasharma3064@gmail.com>
2026-01-15 20:53:30 -0500

    optimized solution for both +ves and -ves

--- LongestSubarray.cpp
+++ LongestSubarray.cpp
@@ -16,13 +16,30 @@ class Solution {
     }
     
 };
+
+
 class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
-        int xorr = 0;
-        for(int i =0;i<nums.size();i++){
-            xorr = xorr ^ nums[i];
+  public:
+    int longestSubarray(vector<int>& arr, int k) {
+        // code here
+        map<long long,int>preSumMap;
+        long long sum =0;
+        int maxlen=0;
+        for(int i =0;i<arr.size();i++){
+            sum+=arr[i];
+            if(sum==k){
+                maxlen = max(maxlen,i+1);
+                
+            }
+            long long rem = sum -k;
+            if(preSumMap.find(rem) != preSumMap.end()){
+                int len = i - preSumMap[rem];
+                maxlen = max(maxlen,len);
+            }
+            if(preSumMap.find(sum)==preSumMap.end()){
+                preSumMap[sum]=i;
+            }
         }
-        return xorr;
+        return maxlen;
     }
 };
--- singleNumber.cpp
+++ singleNumber.cpp
@@ -8,3 +8,13 @@ public:
         return xorr;
     }
 };
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xorr = 0;
+        for(int i =0;i<nums.size();i++){
+            xorr = xorr ^ nums[i];
+        }
+        return xorr;
+    }
+};
